8a997b1ba4eb5aed49ab70f9af8d495972320bbd
Emmanuel <emmanj164@gmail.com>
2025-05-07 21:41:20 +0200

    Move models into correct folder

---
+++ models/MenuItem.js
@@ -0,0 +1,10 @@
+﻿const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: String,
+  category: String
+});
+
+module.exports = mongoose.model('MenuItem', menuItemSchema);
