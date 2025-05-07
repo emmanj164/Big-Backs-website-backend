9126320c69ddc055c97f88dea2fbaffac938a1cb
Emmanuel <emmanj164@gmail.com>
2025-05-07 20:45:36 +0200

    Replaced bcrypt with bcryptjs

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 ﻿const mongoose = require('mongoose');
-require('dotenv').config();
 
-async function initialize() {
+// Connect to MongoDB
+const connectDB = async () => {
   try {
+    // Use the MongoDB URI from your .env file
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('✅ MongoDB connected');
-  } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
-    process.exit(1);
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+    process.exit(1); // Exit the process with failure code
   }
-}
+};
 
-module.exports = { initialize };
+module.exports = { connectDB };
